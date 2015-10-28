50c2f4989112bf14fea3d20bc97931e2e6ee2604
Ryker Blunck <rykerblunck@gmail.com>
2015-10-27 23:19:59 -0600

    bootstrap is works

--- app/js/main.jsx
+++ app/js/main.jsx
@@ -1,3 +1,5 @@
+require('../style/main.scss');
+
 import React from 'react';
 import Layout from './components/Layout.jsx';
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  entry: "./app/js/main.jsx",
+  entry: './app/js/main.jsx',
   output: {
     path: './app/js/',
-    filename: "app.js"
+    filename: 'app.js'
   },
   module: {
     loaders: [
@@ -10,10 +10,30 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel'
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test: /\.woff2?(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=image/svg+xml'
       }
     ]
   },
   devServer: {
     contentBase: './app'
   }
-}
+};
